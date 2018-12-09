5e36a8f8b6ea5f882c5e5c1a199b837d28754372
ricard.arman.kuodis <ricard.arman.kuodis@accenture.com>
2018-12-10 00:58:42 +0200

    Switched from groovy to Kotlin DSL for gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
+import java.net.URI
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.10'
+    val kotlinVersion by rootProject.extra { "1.3.10" }
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.3.0-alpha13'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.4.0-alpha07")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -16,13 +18,16 @@ buildscript {
 }
 
 allprojects {
+
     repositories {
         google()
         jcenter()
-        maven { url 'https://jitpack.io' }
+        maven { url = URI("https://jitpack.io") }
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
 }
